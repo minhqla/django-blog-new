be85b142c8071c8fddbf5b753a71cf2116eb7c67
minhqla <minhqla92@gmail.com>
2026-09-09 15:14:30 +0100

    Add About App

--- blog/views.py
+++ blog/views.py
@@ -29,6 +29,6 @@ def post_detail(request, slug):
     return render(
         request,
         "blog/post_detail.html",
-        {"post": post},
+        {"post": post,
         "coder": "ML"},
     )
