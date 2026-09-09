2098bbdfa57440e9e94c8825a05c08f88d0cba05
minhqla <minhqla92@gmail.com>
2026-09-10 00:37:46 +0100

    Add collaborate request

--- codestar/settings.py
+++ codestar/settings.py
@@ -27,7 +27,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['.herokuapp.com', 'localhost', '127.0.0.1']
 
@@ -48,6 +48,8 @@ INSTALLED_APPS = [
     'blog',
     'django_summernote',
     'about',
+    'crispy_forms',
+    'crispy_bootstrap5',
 ]
 
 SITE_ID = 1
